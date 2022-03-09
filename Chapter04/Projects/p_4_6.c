25b55822c7f8ad052e0249413a2be7a7c0d82386
mayureshpatel <mayureshpatel1795@gmail.com>
2022-03-09 13:48:45 -0500

    Started project six, but keep getting segmentation faults

--- Chapter04/Projects/p_4_6.c
+++ Chapter04/Projects/p_4_6.c
@@ -19,3 +19,50 @@
  *      Compute the remainder when the adjusted total is divided by 10.
  *      Subtract the remainder from 9.
  */
+#include <stdio.h>
+
+int main(void) {
+    // Variables
+    int checkDigit, itemType, manufacturerCode, productCode;
+    int manufacturerDigit1, manufacturerDigit2, manufacturerDigit3, manufacturerDigit4, manufacturerDigit5, manufacturerDigit6;
+    int productDigit1, productDigit2, productDigit3, productDigit4, productDigit5, productDigit6;
+    int firstSum, secondSum, total;
+    
+    // Get the user input for the EAN
+    printf("Enter a 13-digit EAN (#-######-######): ");
+    fflush(stdout);
+
+    scanf("%1d-%6d-%6d", itemType, manufacturerCode, productCode);
+    printf("itemType: %d\n", itemType);
+    printf("manufacturerCode: %d\n", manufacturerCode);
+    printf("productCode: %d\n", productCode);
+
+    // Figure out all the digits in the manufacturer and product codes
+    manufacturerDigit6 = manufacturerCode % 10;
+    manufacturerDigit5 = (manufacturerCode / 10) % 10;
+    manufacturerDigit4 = (manufacturerCode / 100) % 10;
+    manufacturerDigit3 = (manufacturerCode / 1000) % 10;
+    manufacturerDigit2 = (manufacturerCode / 10000) % 10;
+    manufacturerDigit1 = (manufacturerCode / 100000) % 10;
+
+    productDigit6 = productCode % 10;
+    productDigit5 = (productCode / 10) % 10;
+    productDigit4 = (productCode / 100) % 10;
+    productDigit3 = (productCode / 1000) % 10;
+    productDigit2 = (productCode / 10000) % 10;
+    productDigit1 = (productCode / 100000) % 10;
+
+    // Calculate the totals
+    firstSum = manufacturerDigit1 + manufacturerDigit3 + manufacturerDigit5 + productDigit1 + productDigit3 + productDigit5;
+    secondSum = itemType + manufacturerDigit2 + manufacturerDigit4 + manufacturerDigit6 + productDigit2 + productDigit4 + productDigit6;
+    total = (3 * firstSum) + secondSum;
+    
+    // Calculate the check digit
+    checkDigit = 9 - (total % 10);
+
+    printf("Check Digit: %d", checkDigit);
+
+    // exit the program explicitly
+    return 0;
+}
+
