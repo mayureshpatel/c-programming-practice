04141ef9fc8c0eb76247c299810fb53b1bb0f2b7
mayureshpatel <mayureshpatel1795@gmail.com>
2022-03-13 00:07:49 -0500

    Completed project 4

--- Chapter05/Projects/p_6_4.c
+++ Chapter05/Projects/p_6_4.c
@@ -21,3 +21,38 @@
  * Write a program that asks the user to enter a wind speed (in knots),
  * then displays the corresponding description.
  */
+#include <stdio.h>
+
+int main(void) {
+    // Variables
+    float windSpeed;
+
+    // Get user input
+    printf("Enter the wind speed in knots: ");
+    fflush(stdout);
+    scanf("%f", &windSpeed);
+
+    // Calculate the wind description based on the Beaufort scale
+    printf("Wind Speed on Beaufort Scale: ");
+    if(windSpeed < 1) {
+        printf("Calm\n");
+    }
+    else if(windSpeed < 4) {
+        printf("Light Air\n");
+    }
+    else if(windSpeed < 28) {
+        printf("Breeze\n");
+    }
+    else if(windSpeed < 48) {
+        printf("Gale\n");
+    }
+    else if(windSpeed < 63) {
+        printf("Storm\n");
+    }
+    else {
+        printf("HUrricane\n");
+    }
+
+    // exit the program
+    return 0;
+}
