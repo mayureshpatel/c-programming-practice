eee3781e3602acfea71f8ac8ed188457180534f4
mayureshpatel <mayureshpatel1795@gmail.com>
2022-03-23 12:32:45 -0400

    Completed project 10

--- Chapter05/Projects/p_6_10.c
+++ Chapter05/Projects/p_6_10.c
@@ -16,3 +16,40 @@
  *  Output:
  *      Letter grade: B
  */
+#include <stdio.h>
+
+int main(void) {
+    // Variables
+    int numericalGrade;
+
+    // Get user input
+    printf("Enter numerical grade: ");
+    fflush(stdout);
+    scanf("%d", &numericalGrade);
+
+    // Switch statement
+    printf("Letter Grade: ");
+    switch(numericalGrade / 10) {
+        case 10:
+            printf("A\n");
+            break;
+        case 9:
+            printf("A\n");
+            break;
+        case 8:
+            printf("B\n");
+            break;
+        case 7:
+            printf("C\n");
+            break;
+        case 6:
+            printf("D\n");
+            break;
+        default:
+            printf("F\n");
+            break;
+    }
+    
+    // exit the program
+    return 0;
+}
