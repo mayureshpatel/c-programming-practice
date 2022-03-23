bf04cf4904df8851a2e87b89a70fa070610d41df
mayureshpatel <mayureshpatel1795@gmail.com>
2022-03-23 11:29:44 -0400

    Completed project 9

--- Chapter05/Projects/p_6_9.c
+++ Chapter05/Projects/p_6_9.c
@@ -17,3 +17,49 @@
  *  Output:
  *      5/17/07 is earlier than 3/6/08     
  */
+#include <stdio.h>
+
+int main(void) {
+    // Variables
+    int firstMonth, firstDay, firstYear;
+    int secondMonth, secondDay, secondYear;
+
+    // Get user input
+    printf("Enter first date (mm/dd/yy): ");
+    fflush(stdout);
+    scanf("%2d/%2d/%2d", &firstMonth, &firstDay, &firstYear);
+
+    printf("Enter second date (mm/dd/yy): ");
+    fflush(stdout);
+    scanf("%2d/%2d/%2d", &secondMonth, &secondDay, &secondYear);
+    
+    // Decide which date comes first
+    if(firstYear < secondYear) {
+        printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
+    }
+    else if(firstYear > secondYear) {
+        printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
+    }
+    else {
+        if(firstMonth < secondMonth) {
+            printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
+        }
+        else if(firstMonth > secondMonth) {
+            printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
+        }
+        else {
+            if(firstDay < secondDay) {
+                printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
+            }
+            else if(firstDay > secondDay) {
+                printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
+            }
+            else {
+                printf("%02d/%02d/%02d and %02d/%02d/%02d are the same dates", secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
+            }
+        }
+    }
+
+    // exit the program
+    return 0;
+}
